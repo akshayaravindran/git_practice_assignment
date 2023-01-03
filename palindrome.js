3746fcacc74deba1144916894ace5cb94a02561e
akshayaravindran <akshayaravindran1996@gmail.com>
2023-01-03 18:54:34 +0530

    created prime and palindrome files

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+let str= "naman"
+let rev=""
+
+for(let i=str.length-1;i>=0;i--){
+
+rev+= str[i];
+}
+console.log(rev==str)
---
+++ prime.js
@@ -0,0 +1,15 @@
+
+let i=2;
+
+while(i<num){
+if(num%i==0){
+console.log("No)
+break;
+}
+i++;
+
+}
+if (i===num){
+console.log("yes)
+
+};
